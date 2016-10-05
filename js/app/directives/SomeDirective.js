08c7f34726952338669acf81b9ed4c3213b13af1
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-10-05 11:14:32 -0700

    done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,18 +1,23 @@
 function SomeDirective() {
-	return {
-		template: [
-			'<div>',
-				'<h3>{{ counter }}</h3>',
-			'</div>'
-		].join(''),
-		controller: function($scope) {
-			$scope.counter = 0;
-		},
-		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
-
+		return {
+			template: [
+				'<div>',
+					'<h3>{{ ctrl.counter }}</h3>',
+				'</div>'
+			].join(''),
+			controller: function($scope) {
+				this.counter = 0;
+			},
+			controllerAs: 'ctrl',
+			require: 'someDirective',
+			link: function (scope, elem, attrs, ctrl) {
+				document.addEventListener('keydown', function () {
+					ctrl.counter++;
+					elem[0].querySelector('h3').textContent = ctrl.counter;
+					scope.$apply();
+				});
+			}
 		}
-	}
 }
 
 angular
